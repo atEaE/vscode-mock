b6c3aa42bdd60a270eb87c82e33a63e50a836c71
eta aoki <imposter.kore2626@gmail.com>
2021-03-26 12:52:06 +0900

    chore: update.

--- src/mocks/tests/vscodeWorkspaceConfiguration.test.ts
+++ src/mocks/tests/vscodeWorkspaceConfiguration.test.ts
@@ -1,4 +1,5 @@
 // target modules.
+import { assert } from 'chai';
 import { IVSCodeWorkspaceConfiguration } from '../../models';
 import { MockVSCodeWorkspaceConfiguration } from '../vscode';
 
@@ -12,4 +13,21 @@ describe('MockVSCodeWorkspaceConfiguration Tests', () => {
       const mock: IVSCodeWorkspaceConfiguration = new MockVSCodeWorkspaceConfiguration(inner);
     });
   });
+
+  describe('function has check', () => {
+    // arrange
+    let inner = new Map<string, any>();
+    inner.set('key1', 'key1_get_ok');
+    inner.set('key2', 'key2_get_ok');
+    const mock = new MockVSCodeWorkspaceConfiguration(inner);
+
+    it('has return true(exist key)', () => {
+      assert.isTrue(mock.has('key1'));
+      assert.isTrue(mock.has('key2'));
+    });
+    it('has return false(not exist key)', () => {
+      assert.isFalse(mock.has('key3'));
+      assert.isFalse(mock.has('key4'));
+    });
+  })
 });
